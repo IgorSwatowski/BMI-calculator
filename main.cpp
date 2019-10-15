d02ee0b6a8f3a54e40f11e9075468b08fdce2e51
Igor Swatowski <34008204+IgorSwatowski@users.noreply.github.com>
2019-10-15 15:39:13 +0200

    I rebuilt the entire program to make it look even more readable
    I rebuilt the entire program to make it look even more readable. I have combined the recurring functions into one

--- main.cpp
+++ main.cpp
@@ -1,60 +1,53 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <conio.h>
-#include <math.h>
 
 using namespace std;
 
-float odpowiedz1, odpowiedz2, BMI;
+float BMI;
+
+bool czy_w_zakresie(double liczba, double min, double max)
+{
+  return ((liczba >= min) && (liczba < max));
+}
 
 
-int zapytaj_o_liczbe_wzrostu(const string &pytanie1, int odIluWzrost, int doIluWzrost)
+int zapytaj_o_liczbe(const string &nazwa, float min, float max)
 {
-    cout << pytanie1;
+    cout << nazwa << ": ";
+    int odpowiedz;
     for(;;)
     {
-        cin >> odpowiedz1;
-        if((odpowiedz1 >= odIluWzrost) and (odpowiedz1 <= doIluWzrost)) break;
-        else cout << "Wzrost musi sie miescic w przedziale od " << odIluWzrost << " do " << doIluWzrost;
+        cin >> odpowiedz;
+        if( czy_w_zakresie(odpowiedz,min,max) ) break;
+        else cout << nazwa << "musi sie miescic w przedziale od " << min << " do " << max;
     }
-    return odpowiedz1;
+    return odpowiedz;
 }
-int zapytaj_o_liczbe_wagi(const string &pytanie2, int odIluWaga, int doIluWaga)
-{
-    cout << pytanie2;
-    for(;;)
+
+void liczBMI(float wzrost, float waga)
     {
-        cin >> odpowiedz2;
-        if((odpowiedz2 >= odIluWaga) and (odpowiedz2 <= doIluWaga)) break;
-        else cout << "Waga musi sie miescic w przedziale od " << odIluWaga << " do " << doIluWaga;
-    }
-    return odpowiedz2;
-}
- void liczBMI()
-    {
-        BMI = odpowiedz2 / (odpowiedz1*odpowiedz1/10000.);
+        BMI = waga / (wzrost*wzrost/10000.);
         cout << "Twoje BMI jest rowne: " << BMI << endl;
     }
 
 void sprawdzBMI()
 {
-    if(BMI < 16 ) cout << "Wyglodzenie. Bardzo skrajne. Zacznij jesc!" << endl;
-    if(BMI >= 16 && BMI < 16.99) cout << "Twoj organizm jest wychudzony!" << endl;
-    if(BMI >= 17 && BMI < 18.49) cout << "Masz niedowage, zacznij jesc"<< endl;
-    if(BMI >=18.5 && BMI <24.99) cout << "Twoje BMI jest IDEALNE!"<<endl;
-    if(BMI >=25.0 && BMI <29.99) cout << "Masz nadwage! Schudnij troche przyjacielu :D"<<endl;
-    if(BMI >=30.0 && BMI <34.99) cout << "Masz I stopien otylosci! Czas sie przebadac"<<endl;
-    if(BMI >=35.0 && BMI <39.99) cout << "Masz II stopien otylosci! Przebadaj sie koniecznie i zacznij sie ruszac!"<<endl;
-    if(BMI >=40) cout << "III stopien otylosci! Jest to otylosc skrajna. Jak najszybciej udaj sie do lekarza na niezbedne badania i do dietetyka!"<<endl;
-
+    if( BMI < 16) cout << "Jest to bardzo niepokojace i radze jak naszybciej siegnac do lodowki bo jestes wyglodzony!"<<endl;
+    if( czy_w_zakresie(BMI, 16, 17) )    cout << "Twoj organizm jest wychudzony!" << endl;
+    if( czy_w_zakresie(BMI, 17, 18.5)) cout << "Masz niedowage, zacznij jesc"<< endl;
+    if( czy_w_zakresie(BMI, 18.5, 25)) cout << "Twoje BMI jest IDEALNE!"<<endl;
+    if( czy_w_zakresie(BMI, 25, 30))    cout << "Masz nadwage! Schudnij troche przyjacielu :D"<<endl;
+    if( czy_w_zakresie(BMI, 30, 35))    cout << "Masz I stopien otylosci! Czas sie przebadac"<<endl;
+    if( czy_w_zakresie(BMI, 35, 40))    cout << "Masz II stopien otylosci! Przebadaj sie koniecznie i zacznij sie ruszac!"<<endl;
+    if( BMI >= 40 ) cout << "III stopien otylosci! Jest to otylosc skrajna. Jak najszybciej udaj sie do lekarza na niezbedne badania i do dietetyka!"<<endl;
 }
+
 int main()
 {
-    zapytaj_o_liczbe_wzrostu("Podaj wzrost: ",120,220);
-    zapytaj_o_liczbe_wagi("Podaj wage: ",50,300);
-    liczBMI();
-    sprawdzBMI();
+     float wzrost = zapytaj_o_liczbe("Podaj wzrost: ", 120, 220);
+     float waga = zapytaj_o_liczbe("Podaj wage: ", 40, 300);
+     liczBMI(wzrost, waga);
+     sprawdzBMI();
 
     return 0;
 }

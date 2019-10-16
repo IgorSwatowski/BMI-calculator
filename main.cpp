07c691e1e620cabc01a129f26196c7c7235d8749
Igor Swatowski <34008204+IgorSwatowski@users.noreply.github.com>
2019-10-16 17:21:18 +0200

    Small change and added select menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
+
 
 float BMI;
 
@@ -9,7 +11,6 @@ bool czy_w_zakresie(double liczba, double min, double max)
 {
   return ((liczba >= min) && (liczba < max));
 }
-
 
 int zapytaj_o_liczbe(const string &nazwa, float min, float max)
 {
@@ -42,12 +43,42 @@ void sprawdzBMI()
     if( BMI >= 40 ) cout << "III stopien otylosci! Jest to otylosc skrajna. Jak najszybciej udaj sie do lekarza na niezbedne badania i do dietetyka!"<<endl;
 }
 
+void menuWyboru()
+{
+    cout << "-------------------------------------" << endl;
+    cout << "Witaj w programie liczacym BMI!"<<endl;
+    cout << "-------------------------------------"<<endl;
+    int wybor;
+    cout << "1) Czym jest BMI?"<<endl << "2) Wazne informacje"<<endl << "3) Przejdz do kalkulatora"<<endl;
+    cin >> wybor;
+    system("cls");
+    switch(wybor)
+    {
+        case 1:
+            cout << "BMI jest jednym z waznych wskazniow okreslajacych nasz stan fizyczny, ale niestety nie wystarczajacym. Bardzo waznym uzupelnieniem BMI jest wskaznik ilosci tluszczu brzusznego - zbyt duzy moze oznaczac niebezpieczna otylosc brzuszna";
+            cout << "i to nawet przy prawidlowym BMI! Ponadto, paradoksalnie, badania naukowe wskazuja, ze osoby z lekka nadwaga zwykle sa zdrowsze i zyja dluzej od osob z tzw. prawidlowa waga.";
+            cout << "Pojawiaja sie nawet glosy, ze ustalony arbitralnie przez WHO prog nadwagi (25) jest zbyt niski."<<endl;
+        break;
+        case 2:
+            cout << "Kalkulator BMI obrazuje przyblizona zawartosc tluszczu w organizmie. W przypadku niektorych osob wskaznik BMI moze sugerowac bledne wnioski. ";
+            cout << "Osoby aktywne fizycznie, uprawiajacy sport, moga posiadac zawyzona wage zwiazana z tkanka miesniowa a nie z iloscia tluszczu w organizmie.";
+            cout << "Ponadto nie zaleca sie stosowania wskaznika BMI do oznaczania wagi ciala dla dzieci do ok. 14 roku zycia oraz dla kobiet w ciazy."<<endl;
+        break;
+        case 3:
+             float wzrost = zapytaj_o_liczbe("Podaj wzrost: ", 120, 220);
+             float waga = zapytaj_o_liczbe("Podaj wage: ", 40, 300);
+             liczBMI(wzrost, waga);
+             sprawdzBMI();
+        break;
+    }
+}
+
 int main()
 {
-     float wzrost = zapytaj_o_liczbe("Podaj wzrost: ", 120, 220);
-     float waga = zapytaj_o_liczbe("Podaj wage: ", 40, 300);
-     liczBMI(wzrost, waga);
-     sprawdzBMI();
+    HANDLE kolor;
+    kolor = GetStdHandle( STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(kolor, 10);
+    menuWyboru();
 
     return 0;
 }
